bbe7fc3544e5cce4bf01aabd163c85bebfdfe531
Gabriel <gabriel.batalha00@gmail.com>
2022-10-10 11:31:21 +0100

    Added attemps system and handling user guesses

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Objective: make a hangman application
 
 To do:
 
-Random word selector
-Input from user
+Random word selector - Done;
+Attemps system - Done;
+Input from user- Done;
 Visual of hangman
-Attemps system
+Protecting buffer overuns
+refactor code into different functions
--- main.c
+++ main.c
@@ -3,18 +3,34 @@
 #include <time.h>
 #include <string.h>
 
+int letterCheck(char c, char *word)
+{
+    int i;
+    i = 0;
+    while (word[i])
+    {
+        if (word[i] == c)
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
 int main(void)
 {
     int randomNumber;
     char word[20];
     int i;
-    // opens file
+    char guess;
+    int len;
+    int j;
+   // opens file
     FILE *wordFile = fopen("words.txt", "r");
     // check if file open was succesful
     if (wordFile == NULL)
     {
         printf("File could not be opened");
-        return (1);
+       return (1);
     }
     // generating a random number to pick the word
     srand(time(0));
@@ -23,8 +39,45 @@ int main(void)
     i = 0;
     while (i++ != randomNumber)
         fgets(word, 20, (FILE*) wordFile);
-    // get letter from user
-
-
+    i = 0;
+    // removing new line character
+    len = strlen(word);
+    while (i++ < len)
+    {
+       if (word[i] == '\n')
+        word[i] = '\0';
+    }
+   fclose(wordFile);
+    //printf("%s\n", word);
+    i = 1;
+    j = 0;
+    // looping through max numbers of trials 
+    while (i <= 7)
+    {
+        if (i == 6) // checking if tries are at max
+        {
+            printf("Woops! You lost\n");
+            break;
+        }
+        printf("Guess a letter:\n");
+        scanf(" %c", &guess); // getting guess form user 
+        if (letterCheck(guess, word) == 1) // checking if letter exists in word
+        {
+            if (j == len)
+            {
+                printf("GG!");
+                break;
+            }
+            printf("You're right!\nGo again:)\n");
+            j++;
+            i++;
+        }
+        else // giving another try
+         {
+            printf("This letter is not in the word :C\n");
+            printf("This is your %d try, you have 6 in total\n", i);
+            i++;
+         }
+    }
     return(0);
 }
